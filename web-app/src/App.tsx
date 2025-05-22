0a56eec47310c58073e361ed077c1faffb0e5b9b
Gerald Gugger <gerald.gugger@gmail.com>
2025-05-22 16:38:06 -0400

    Fix TypeScript errors - remove unused variables

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Chat from './components/chat';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
